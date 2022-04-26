d1a9ff31191a8cb81b5db415e41c5cf8380374d8
Ines Souza <inessouzasp@gmail.com>
2022-04-25 21:58:33 -0300

    JSON

---
+++ cursos/nivelamento-javascript-dslearn/json/readme.md
@@ -0,0 +1,10 @@
+JSON
+
+A sigla JSON significa JavaScript Object Notation.
+
+Exemplo Síntaxe:
+
+const objectJSON = {
+    "product": "Computador",
+    "price": 3000.00
+}
---
+++ cursos/nivelamento-javascript-dslearn/json/script.js
@@ -0,0 +1,26 @@
+const objectOne = {
+    nome: "Computador",
+    preco: 3000.00
+}
+
+const objectTwo = {
+    id: 1,
+    date: "2022-04-25",
+    items: [
+        {
+            description: "Celular",
+            price: 1499.99,
+            quantity: 1
+        },
+        {
+            description: "Mouse",
+            price: 100.99,
+            quantity: 1
+        }
+    ],
+    client: {
+        name: "Maria Red",
+        email: "maria@gmail.com",
+        active: true
+    }
+}
