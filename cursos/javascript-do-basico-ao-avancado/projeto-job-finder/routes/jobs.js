332b8488804e14cccce86d0b0f9c05b49e4a7d84
inessouza <ines.souza@webcore.com.br>
2025-02-27 23:39:39 -0300

    descrição de rotas

--- cursos/javascript-do-basico-ao-avancado/projeto-job-finder/routes/jobs.js
+++ cursos/javascript-do-basico-ao-avancado/projeto-job-finder/routes/jobs.js
@@ -2,14 +2,17 @@ const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
 
+// rota teste
 router.get('/test', (req, res) => {
     res.send('ok')
 })
 
+// rota para adicionar dados
 router.post('/add', (req, res) => {
     let { title, salary, company, email, new_job } = req.body
     Job.create({
         title,
+        description,
         salary,
         company,
         email,
